45b502ce82a6f95f1db75909ea3f113fce7e67b3
adeoyewole028 <adeoyewole028@gmail.com>
2022-09-08 12:37:43 +0100

    fixed transition

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Illustration from "./Illustration";
-import Nav from "./Navbar";
+import Navbar from "./Navbar";
 import Values from "./Values";
 
 const Header = () => {
   return (
     <div className="pb-10 lg:pb-32 px-3 lg:px-10 pt-7">
-      <Nav />
+      <Navbar />
       <div className="text-white flex flex-col md:flex-row md:justify-between pt-10">
         <Values />
         <Illustration />
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -21,11 +21,10 @@ const navMenu = [
   },
 ];
 
-const MobileNav = () => {
+const Nav = () => {
   const [trans] = useState(
-    "flex flex-col bg-slate-800 fixed w-full h-full text-3xl overflow-hidden right-0 bottom-0 top-0 z-30 transition-all ease-in-out duration-300"
+    "fixed top-0 bottom-0 z-30 w-full transition-all ease-in duration-500 text-3xl"
   );
-
 
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(!show);
@@ -64,7 +63,7 @@ const MobileNav = () => {
           <div className="lg:hidden z-40">
             {show ? (
               <FaTimes
-                className="text-2xl text-white cursor-pointer"
+                className="text-2xl text-white cursor-pointer fixed top-16 right-3"
                 onClick={handleShow}
               />
             ) : (
@@ -76,38 +75,40 @@ const MobileNav = () => {
           </div>
         </div>
       </div>
-      {show && (
-        <div className={trans} style={{ width: show ? "100%" : "0" }}>
-          <ul className="flex mt-40 flex-col items-center space-y-8 py-5">
-            {navMenu.map((item, index) => (
-              <li key={index}>
-                <a href={item.link} className="text-white text-lg">
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <div className="flex space-x-5 self-center text-white">
-            <a href="/">Login</a>
+      <div className={`${trans} ${show ? "-right-0" : "-right-full"}`}>
+        {show && (
+          <div className="flex flex-col bg-slate-800  h-full w-full">
+            <ul className="flex mt-40 flex-col items-center space-y-8 py-5">
+              {navMenu.map((item, index) => (
+                <li key={index}>
+                  <a href={item.link} className="text-white text-lg">
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-5 self-center text-white">
+              <a href="/">Login</a>
 
-            <div className="text-zinc-500">|</div>
+              <div className="text-zinc-500">|</div>
 
-            <a href="/" className="bg-blue-600 rounded-full px-3">
-              Register
-            </a>
+              <a href="/" className="bg-blue-600 rounded-full px-3">
+                Register
+              </a>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   );
 };
 
-const Nav = () => {
+const Navbar = () => {
   return (
     <div>
-      <MobileNav />
+      <Nav />
     </div>
   );
 };
 
-export default Nav;
+export default Navbar;
